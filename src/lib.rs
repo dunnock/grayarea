594251a2d6bf02afb3b99b5e3f4db3218cc9b2a3
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2019-12-13 10:29:51 +0200

    start adding websockets via tunstenite tokio

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
-use wasmer_runtime::{WasmPtr, Array, Ctx};
+mod websocket;
+pub use websocket::websocket_send_message;
 
-pub fn websocket_send_message(ctx: &mut Ctx, message_ptr: WasmPtr<u8, Array>, len: u32) {
-	let memory = ctx.memory(0);
-	let message = message_ptr.get_utf8_string(memory, len);
-    println!("{:?}", message);
+use wasmer_runtime::{Memory, types::WasmExternType};
+
+#[derive(Clone, Copy)]
+pub struct U8WasmPtr {
+	offset: u32
 }
+unsafe impl WasmExternType for U8WasmPtr {
+    type Native = i32;
 
+    fn to_native(self) -> Self::Native {
+        self.offset as i32
+    }
+    fn from_native(n: Self::Native) -> Self {
+        Self {
+            offset: n as u32
+        }
+    }
+}
+impl U8WasmPtr {
+    /// Get a UTF-8 string representation of this `WasmPtr` with the given length.
+    fn get_slice<'a>(self, memory: &'a Memory, len: u32) -> Option<&'a [u8]> {
+        if self.offset as usize + len as usize > memory.size().bytes().0 {
+            return None;
+        }
+        let ptr = unsafe { memory.view::<u8>().as_ptr().add(self.offset as usize) as *const u8 };
+        Some(unsafe { std::slice::from_raw_parts(ptr, len as usize) })
+    }
+}
---
+++ src/websocket.rs
@@ -0,0 +1,45 @@
+use wasmer_runtime::{Ctx};
+use std::cell::RefCell;
+use super::U8WasmPtr;
+pub use tungstenite::protocol::Message;
+use tungstenite::error::Error;
+use futures::SinkExt;
+use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream};
+use url;
+
+pub struct WebSocket {
+	stream: Option<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>
+}
+
+thread_local! {
+    pub static WS: RefCell<WebSocket> = RefCell::new(WebSocket::new());
+}
+
+impl WebSocket {
+	pub fn new() -> Self {
+		WebSocket { stream: None }
+	}
+	pub async fn connect(&mut self, addr: url::Url) 
+		-> Result<(), Error> 
+	{
+		let (mut ws_stream, _) = connect_async(addr).await?;
+		WS.with(|ws| { ws.borrow_mut().stream = Some(ws_stream); });
+		Ok(())
+	}
+	pub async fn send_message(message: &[u8]) {
+		WS.with(|ws| { 
+/*			if let Some(&mut stream) = ws.borrow().stream.as_mut() {
+				stream.send(Message::binary(message));
+			}; */
+		});
+		()	
+	}
+}
+
+pub fn websocket_send_message(ctx: &mut Ctx, message_ptr: U8WasmPtr, len: u32) {
+	let memory = ctx.memory(0);
+	let message = message_ptr.get_slice(memory, len);
+	println!("{:?}", message);
+	WebSocket::send_message(message.unwrap());
+}
+
