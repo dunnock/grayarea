49ae9e01f77943d94db2fed844da9f224adcba07
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2019-12-17 13:57:44 +0200

    refactor packages, move runtime to separate package

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
-mod websocket;
 mod wasm;
 mod options;
 mod config;
 mod ptr;
 mod output;
-pub use websocket::WebSocket;
 pub use wasm::WasmInstance;
 pub use options::Opt;
 pub use config::Config;
 pub use ptr::U8WasmPtr;
 pub use output::Output;
+
+// WebSocket module support
+#[cfg(feature="ws")]
+mod websocket;
+#[cfg(feature="ws")]
+pub use websocket::WebSocket;
