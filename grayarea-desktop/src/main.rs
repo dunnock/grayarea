7ff6472e490c09d41516fff06776651fafdd886a
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-01-31 15:11:36 +0200

    cleanup stuff

--- grayarea-desktop/src/main.rs
+++ grayarea-desktop/src/main.rs
@@ -1,17 +1,11 @@
 #![allow(clippy::unnecessary_mut_passed)]
 
-use crossbeam::channel;
 use futures::future::try_join_all;
 use grayarea_desktop::Opt;
-use ipc_orchestrator::{message::Message, orchestrator};
-use std::collections::HashMap;
+use ipc_orchestrator::{orchestrator};
 use structopt::StructOpt;
 use tokio::process::Command;
 use grayarea::config::Input;
-
-const CHANNEL_SIZE: usize = 10;
-
-type TopicChannel = (channel::Sender<Message>, channel::Receiver<Message>);
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -20,16 +14,6 @@ async fn main() -> anyhow::Result<()> {
     let config = opt.load_config().await?;
     // Load all modules configs
     let modules = try_join_all(config.functions.iter().map(|module| module.load_config())).await?;
-    // Create crossbeam channels for all input topics
-    let _in_topics: HashMap<String, TopicChannel> = modules
-        .iter()
-        .filter_map(|module| {
-            module
-                .input
-                .as_ref()
-                .map(|input| (input.topic.clone(), channel::bounded(CHANNEL_SIZE)))
-        })
-        .collect();
 
     // Start out commands
     let mut orchestrator = orchestrator().ipc(true).rust_backtrace(opt.debug);
@@ -50,28 +34,6 @@ async fn main() -> anyhow::Result<()> {
     // Estiblish connections between commands
     let mut orchestra = orchestrator.connect().await?;
     for module in modules.into_iter() {
-        // Connect module's outputs to relevant topics
-        // Will fail if some output topics have not relevant sinks
-        /*
-        if let Ok(topics) = module.topics() {
-            let mut out_topics = HashMap::new();
-            for name in topics {
-                let topic = in_topics.get(&name).ok_or_else(|| {
-                    anyhow::anyhow!("topic {} is not pointing to any function input", name)
-                })?;
-                out_topics.insert(name, topic.0.clone());
-            }
-            orchestra.forward_bridge_rx(&module.name, out_topics)?;
-        }
-        // Connect module's input to topic
-        if let Some(Some(topic)) = module
-            .input
-            .as_ref()
-            .map(|input| in_topics.get(&input.topic))
-        {
-            orchestra.forward_bridge_tx(&module.name, topic.1.clone())?;
-        }
-        */
         if let Some(Input {topic,..}) = module.input.as_ref()
         {
             orchestra.route_topic_to_bridge(&topic, &module.name)?;
